7b800512d71188f7a34591db97c5c32f6f7a8d05
Selma <selma.khaje@gmail.com>
2023-11-20 21:49:38 +0330

    Added weather data

--- src/index.js
+++ src/index.js
@@ -1,91 +1,59 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20,
-  },
-};
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windspeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
-let city = prompt("Enter a city?");
-city = city.toLowerCase();
-if (weather[city] !== undefined) {
-  let temperature = weather[city].temp;
-  let humidity = weather[city].humidity;
-  let celsiusTemperature = Math.round(temperature);
-  let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
-
-  alert(
-    `It is currently ${celsiusTemperature}°C (${fahrenheitTemperature}°F) in ${city} with a humidity of ${humidity}%`
-  );
-} else {
-  alert(
-    `Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
-  );
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatData(date);
 }
 
-//or
+function formatData(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-let city = prompt("Enter a city");
-if (city === "paris") {
-  alert(
-    `It is currently ${Math.round(weather.paris.temp)}°C (${Math.round(
-      (weather.paris.temp * 9) / 5 + 32
-    )}°F) in paris with a humidity of ${weather.paris.humidity}%.`
-  );
-} else {
-  if (city === "tokyo") {
-    alert(
-      `It is currently ${Math.round(weather.tokyo.temp)}°C (${Math.round(
-        (weather.tokyo.temp * 9) / 5 + 32
-      )}°F) in tokyo with a humidity of ${weather.tokyo.humidity}%.`
-    );
-  } else {
-    if (city === "lisbon") {
-      alert(
-        `It is currently ${Math.round(weather.lisbon.temp)}°C  (${Math.round(
-          (weather.lisbon.temp * 9) / 5 + 32
-        )}°F) in lisbon with a humidity of ${weather.lisbon.humidity}%.`
-      );
-    } else {
-      if (city === "san francisco") {
-        alert(
-          `It is currently ${Math.round(
-            weather["san francisco"].temp
-          )}°C (${Math.round(
-            (weather["san francisco"].temp * 9) / 5 + 32
-          )}°F) in san francisco with a humidity of ${
-            weather["san francisco"].humidity
-          }%.`
-        );
-      } else {
-        if (city === "oslo") {
-          alert(
-            `It is currently ${Math.round(weather.oslo.temp)}°C (${Math.round(
-              (weather.oslo.temp * 9) / 5 + 32
-            )}°F) in oslo with a humidity of ${weather.oslo.humidity}%.`
-          );
-        } else {
-          alert(
-            "Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+dd"
-          );
-        }
-      }
-    }
+  let Day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${Day} ${hours}:${minutes}`;
 }
-//or
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+searchCity("Tehran");
